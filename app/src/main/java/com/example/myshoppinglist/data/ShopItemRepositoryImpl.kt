faf47d5a24b7e5ea33636d252d0d81228536956a
nikita <paramonov20@bk.ru>
2023-05-24 16:55:50 +0300

    Научился преобразовывать объекты одного слоя в другой, также научился преобразовывать одну liveData в другую, при помощи классов Mediator and Transformations

--- app/src/main/java/com/example/myshoppinglist/data/ShopItemRepositoryImpl.kt
+++ app/src/main/java/com/example/myshoppinglist/data/ShopItemRepositoryImpl.kt
@@ -1,60 +1,51 @@
 package com.example.myshoppinglist.data
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import android.app.Application
+import androidx.lifecycle.*
+import com.example.myshoppinglist.data.database.AppDatabase
+import com.example.myshoppinglist.data.mapper.ShopListMapper
 import com.example.myshoppinglist.domain.ShopItem
 import com.example.myshoppinglist.domain.ShopItem.Companion.UNDEFINED_ID
 import com.example.myshoppinglist.domain.ShopItemRepository
 import kotlin.random.Random
 
-object ShopItemRepositoryImpl : ShopItemRepository {
+class ShopItemRepositoryImpl(
+    application: Application
+) : ShopItemRepository {
 
-    private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = sortedSetOf<ShopItem>({item1, item2 -> item1.id.compareTo(item2.id)})
-
-    private var autoIncrementId = 0
-
-    init {
-        for (i in 0..100) {
-            val item = ShopItem(
-                name = "Name $i",
-                count = i,
-                enabled = Random.nextBoolean(),
-            )
-            addShopItem(item)
-        }
-    }
+    private val shopListDao = AppDatabase.getInstance(application).shopListDao()
+    private val mapper = ShopListMapper()
 
     override fun addShopItem(shopItem: ShopItem) {
-        if (shopItem.id == UNDEFINED_ID) {
-            shopItem.id = autoIncrementId++
-        }
-        shopList.add(shopItem)
-        updateList()
+        shopListDao.addShopItem(mapper.mapShopItemEntityToDbModel(shopItem))
     }
 
     override fun editShopItem(shopItem: ShopItem) {
-        val oldElement = getShopItem(shopItem.id)
-        removeSHopItem(oldElement)
-        addShopItem(shopItem)
+        shopListDao.addShopItem(mapper.mapShopItemEntityToDbModel(shopItem))
     }
 
     override fun removeSHopItem(shopItem: ShopItem) {
-        shopList.remove(shopItem)
-        updateList()
+        shopListDao.removeSHopItem(shopItem.id)
     }
 
+    // первый способ преоборазования одной liveData в другую
+//    override fun getShopList(): LiveData<List<ShopItem>> = MediatorLiveData<List<ShopItem>>().apply {
+//        addSource(shopListDao.getAllShopList()) { shopListDbModel ->
+//            value = mapper.mapShopListDbModelToEntity(shopListDbModel)
+//        }
+//    }
+    // второй способ
     override fun getShopList(): LiveData<List<ShopItem>> {
-        return shopListLD
+        val allShopList = shopListDao.getAllShopList()
+        return allShopList.switchMap { shopItemDbModels ->
+            MutableLiveData<List<ShopItem>>().apply {
+                value = mapper.mapShopListDbModelToEntity(shopItemDbModels)
+            }
+        }
     }
 
     override fun getShopItem(shopItemId: Int): ShopItem {
-        return shopList.find {
-            it.id == shopItemId
-        }?: throw RuntimeException("Element with id $shopItemId not found")
-    }
-
-    private fun updateList() {
-        shopListLD.value = shopList.toList()
+        val dbModel = shopListDao.getShopItem(shopItemId)
+        return mapper.mapShopItemDbModelToEntity(dbModel)
     }
 }
